1cc4c1da35324cfbd88b8d80c0b60c039f77b93e
fer <fgomez2865@gmail.com>
2020-09-15 21:43:22 +0200

    extract data using lxml

--- cnmv/get_vl.py
+++ cnmv/get_vl.py
@@ -3,6 +3,8 @@ import requests
 from urllib.parse import urljoin
 import os
 
+import zipfile 
+from lxml import etree
 
 def download_file (year, month, url):
     print (f"{month = }, {url =}")
@@ -14,11 +16,9 @@ def download_file (year, month, url):
 def get_files (year):
     url = f"https://www.cnmv.es/portal/Publicaciones/Descarga-Informacion-Individual.aspx?ejercicio={year}"
 
-
     # head obtiene el tamaño del fichero para poder comparar
     resp = requests.get(url)
     soup = BeautifulSoup(resp.text)
-
 
     results = soup.select('td[data-th="Documento"]')
 
@@ -29,7 +29,48 @@ def get_files (year):
         month = result.parent.td.text
         relative_url = result.a["href"]
 
-        url_zip = urljoin (url, relative_url)
-        download_file(year, month, url_zip)
+def read_vl(fn):
 
-get_files("2019")
+    tags_text = ["Tipo", "NumerodeRegistro", "NumeroCompartimento", "ISIN"] 
+    tags = ["VLDiario", "ParticipesDiario", "PatrimonioDiario"]
+
+    tags_text = ["Tipo", "NumeroRegistro", "NumeroCompartimento", "ISIN"] 
+    tags_diario = [("VLDiario","VL_Dia"), 
+                  ("ParticipesDiario", "Participes_Dia"),
+                  ("PatrimonioDiario", "Patrimonio_Dia")]
+
+    root = etree.parse(fn)
+    entidades = root.findall("Entidad")
+    for ent in entidades:
+        print (ent)
+
+        for tag in tags_text:
+          texto = ent.find(".//" + tag).text
+          print (texto)
+
+        for tag_root, tag in tags_diario:
+          elm = ent.find(".//" + tag_root)
+
+          elm_diarios = elm.xpath(f".//*[starts-with(name(), '{tag}')]")
+          for elm_diario in elm_diarios:
+                
+            print (f"{elm_diario.tag = }, {elm_diario.text = }")
+
+def main():
+    # get_files("2019")
+
+    fn = "Febrero"
+    zip_fn = fn + ".zip"
+
+    with zipfile.ZipFile(zip_fn,"r") as zip_ref:
+        zip_ref.extractall("/tmp/" + fn)
+
+    fn ="/tmp/Febrero/FONDMENS_202002.xml"
+
+    read_vl (fn)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
