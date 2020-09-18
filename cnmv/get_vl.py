4e45f606479197ce15e0170df18c4a620144ed65
fer <fgomez2865@gmail.com>
2020-09-18 19:19:59 +0200

    load the zip files in the filesystem

--- cnmv/get_vl.py
+++ cnmv/get_vl.py
@@ -1,20 +1,25 @@
-from bs4 import BeautifulSoup
-import requests
-from urllib.parse import urljoin
 import os
-
-import zipfile 
-from lxml import etree
 import sqlite3
+import zipfile
 
-def download_file (year, month, url):
-    print (f"{month = }, {url =}")
+import requests
+from bs4 import BeautifulSoup
+from lxml import etree
+
+MESES = list(('enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio',
+            'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'))
+
+def download_file(year, month, url):
+    print(f"{month = }, {url =}")
 
     r = requests.get(url, allow_redirects=True)
     open(f"{year}/{month}.zip", 'wb').write(r.content)
 
 
-def get_files (year):
+def get_files(year):
+
+    year = str(year)
+
     url = f"https://www.cnmv.es/portal/Publicaciones/Descarga-Informacion-Individual.aspx?ejercicio={year}"
 
     # head obtiene el tamaño del fichero para poder comparar
@@ -27,17 +32,29 @@ def get_files (year):
         os.makedirs(year)
 
     for result in results:
-        month = result.parent.td.text
+        month_name = result.parent.td.text
         relative_url = result.a["href"]
 
-def get_content (elm, tag):
+        os.path.exists(f'{year}/{month_name}')
+
+        r = requests.get(url, allow_redirects=True)
+
+        file_name =f'{year}/{month_name}.zip'
+
+        print (f"----- writing {file_name = } ------")
+        with open(file_name, 'wb') as f:
+            f.write(r.content)
+
+
+
+def get_content(elm, tag):
     query = f".//{tag}"
 
     sub_elm = elm.find(query)
-    if sub_elm  is None:
+    if sub_elm is None:
         return 0
 
-    print (sub_elm.text)
+    print(sub_elm.text)
 
     return sub_elm.text
 
@@ -47,18 +64,16 @@ def read_vl(fn):
     root = etree.parse(fn)
     entidades = root.findall("Entidad")
 
-    conn= sqlite3.connect("cnmv.db")
+    conn = sqlite3.connect("cnmv.db")
     c = conn.cursor()
-
 
     for ent in entidades:
 
-        tipo = get_content (ent, "Tipo")
-        registro = get_content (ent, "NumeroRegistro")
-        compartimento = get_content (ent, "NumeroCompartimento")
-        isin = get_content (ent, "ISIN")
-        clase = get_content (ent, "NumeroClase")
-
+        tipo = get_content(ent, "Tipo")
+        registro = get_content(ent, "NumeroRegistro")
+        compartimento = get_content(ent, "NumeroCompartimento")
+        isin = get_content(ent, "ISIN")
+        clase = get_content(ent, "NumeroClase")
 
         sql = f"INSERT INTO 'info_fondo' VALUES ('{isin}', '{tipo}', '{registro}', {compartimento}, {clase})"
 
@@ -69,17 +84,17 @@ def read_vl(fn):
 
         conn.commit()
 
-        vl = get_content (ent, "VLDiario")
-        participes = get_content (ent, "ParticipesDiario")
-        patrimonio = get_content (ent, "PatrimonioDiario")
+        vl = get_content(ent, "VLDiario")
+        participes = get_content(ent, "ParticipesDiario")
+        patrimonio = get_content(ent, "PatrimonioDiario")
 
-        for dia in range(1,31):
-            vl = get_content (ent, f"VL_Dia{dia}")
-            participes = get_content (ent, f"Participes_Dia{dia}")
-            patrimonio = get_content (ent, f"Patrimonio_Dia{dia}")
-            _date = f"2020-02-{dia}"
+        for dia in range(1, 31):
+            vl = get_content(ent, f"VL_Dia{dia}")
+            participes = get_content(ent, f"Participes_Dia{dia}")
+            patrimonio = get_content(ent, f"Patrimonio_Dia{dia}")
+            fecha = f"2020-02-{dia}"
 
-            sql = f"INSERT INTO 'fondo_diario' VALUES ('{isin}', {_date}, {vl}, '{participes}', {patrimonio})"
+            sql = f"INSERT INTO 'fondo_diario' VALUES ('{isin}', {fecha}, {vl}, '{participes}', {patrimonio})"
 
             print(sql)
 
@@ -90,23 +105,32 @@ def read_vl(fn):
 
             conn.commit()
 
-
     conn.close()
 
+
 def main():
-    # get_files("2019")
 
-    fn = "Febrero"
-    zip_fn = fn + ".zip"
+    for year in range(2010, 2020):
+        get_files (year)
 
-    with zipfile.ZipFile(zip_fn,"r") as zip_ref:
-        zip_ref.extractall("/tmp/" + fn)
+        continue
 
-    fn ="/tmp/Febrero/FONDMENS_202002.xml"
+        for month in range(0, 11):
+            pass
+            # extract(year, month)
 
-    read_vl (fn)
+def extract(year, month):
 
-    return
+    month_name = MESES[month]
+
+    zip_fn = month_name + ".zip"
+
+    with zipfile.ZipFile(zip_fn, "r") as zip_ref:
+        zip_ref.extractall("/tmp/" + month_name)
+
+        month_name = f"/tmp/{month_name}/FONDMENS_{year}{month}.xml"
+
+        read_vl(month_name)
 
 
 if __name__ == "__main__":
